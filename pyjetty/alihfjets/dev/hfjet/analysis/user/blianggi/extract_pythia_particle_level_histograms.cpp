89b98e5c246bdfc1a39621db235b138ddaea07aa
blianggilman <blianggilman@gmail.com>
2026-02-05 14:46:56 -0800

    more fixes

--- pyjetty/alihfjets/dev/hfjet/analysis/user/blianggi/extract_pythia_particle_level_histograms.cpp
+++ pyjetty/alihfjets/dev/hfjet/analysis/user/blianggi/extract_pythia_particle_level_histograms.cpp
@@ -12,7 +12,7 @@
 
 // NOTE: DETECTOR LEVEL IS MORE COMPLICATED -- CAN IMPLEMENT THAT LATER
 
-// RUN LIKE SO: root -l extract_pythia_particle_level_histograms.cpp("h") for herwig or ("p") for pythia
+// RUN LIKE SO: root -l 'extract_pythia_particle_level_histograms.cpp("h")' for herwig or ("p") for pythia
 
 // std::string base_filepath_header = "/global/cfs/projectdirs/alice/alicepro/hiccup";
 
@@ -402,7 +402,7 @@ void compareParticleBranches_TChain(std::ofstream &outfile, TFile * fout_root, G
 void extract_pythia_particle_level_histograms(const char *opts = "") {
 
     // ------- CHOOSE PYTHIA OR HERWIG -------
-    Tstring options(opts);
+    TString options(opts);
     std::string generator_choice;
     if (options.Contains("h")) {
         generator_choice = "herwig"; // "herwig"
@@ -412,10 +412,7 @@ void extract_pythia_particle_level_histograms(const char *opts = "") {
         std::cerr << "Invalid option! Use 'h' for herwig or 'p' for pythia as per instructions at top of this file." << std::endl;
         return;
     }
-
     
-    std::string generator_choice = "herwig"; 
-
     // -------- INPUT DIRECTORIES --------
     // post eff smearing -- generator + detector level
     std::string pythia_prompt_filepaths = "/global/cfs/cdirs/alice/alicepro/hiccup/rstorage/alice/generation/blianggi/pythiagen/tree_fastsim/45178629/45154942/files.txt"; 
